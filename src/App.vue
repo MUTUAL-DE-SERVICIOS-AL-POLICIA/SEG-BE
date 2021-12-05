c7d34b9b112c62f323a32edc029f2a2ec9b25616
gladys850 <gpq850@gmail.com>
2021-12-05 13:00:10 -0400

    reinstalacion de vue router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
- <div>
-    <Main />
- </div>
+  <div id="app">
+    <div id="nav">
+      <Main />
+    </div>
+    <router-view/>
+  </div>
 </template>
-
 <script>
 import Main from '@/components/main.vue'
+
 export default {
   components: {
     Main
-  }
+  },
 }
 </script>
 
-<style>
 
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//import Home from '../views/Home.vue'
+import Main from '@/components/main'
+import Info from '@/components/info'
+
+Vue.use(VueRouter)
+
+const routes = [
+/*  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( //webpackChunkName: "about" // '../views/About.vue')
+  }, 
+  */
+  {
+    name: 'inicio',
+    path: '/inicio',
+    component: Main
+  }, {
+    path: '/info',
+    name: 'info',
+    component: Info,
+
+  }
+]
+
+
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
